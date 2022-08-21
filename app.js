91bf3b7c74d961e8b9701b24e7dfb592e9dc1ec9
Ammar Hussein <ammar.atef45@gmail.com>
2022-08-20 19:24:52 -0700

    Added scheduler to send random questions to registered channels

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ ec2-user@ec2-34-229-13-222.compute-1.amazonaws.com:~/QOTD
 import 'dotenv/config';
 import { CommandFactory } from './command/factory.js';
 import { Client, GatewayIntentBits } from 'discord.js';
+import { ddbDocClient } from "./ddb-doc-client.js";
+import { ScanCommand } from "@aws-sdk/lib-dynamodb";
+import pkg from 'node-cron';
+import { randomUUID } from 'crypto';
+const { schedule } = pkg;
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -16,7 +21,9 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
-  //sendMessageToChannel('991149794712096768', ':p')
+  schedule('0 17 * * *', function() {
+    dailyRoutine();
+  });
 });
 
 client.on('interactionCreate', async interaction => {
@@ -29,10 +36,51 @@ client.on('interactionCreate', async interaction => {
 // Login to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
 
-function sendMessageToChannel(channel_id, message) {
-  client.channels.fetch(channel_id)
-    .then(async channel => {
-      channel.send(message);
-    }
-  ); 
+async function dailyRoutine() {
+  const channels = await getChannels();
+  const question = await getRandomQuestion();
+  for(const channel of channels) {
+    await sendMessageToChannel(channel, buildMessage(question));
+  }
+}
+
+function buildMessage(question) {
+  return `Question of the day:\n`+
+        `${question.question}\n`+
+        `Question was added by: ${question.user}\n\n`+
+        `use slash commadn add-question to add a question to my database`;
+}
+
+async function getRandomQuestion() {
+  const params = {
+    TableName: 'QOTD_questions',
+    ExclusiveStartKey: {
+      'uuid': randomUUID()
+    },
+    Limit: 1
+  };
+  try {
+    const data = await ddbDocClient.send(new ScanCommand(params));
+    return data.Items[0];
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function getChannels() {
+  const params = {
+    TableName: 'QOTD_channels',
+  };
+  try {
+    const data = await ddbDocClient.send(new ScanCommand(params));
+    const values = data.Items.map(x=> x.channel);
+    return values;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function sendMessageToChannel(channel_id, message) {
+  const channel = await client.channels.fetch(channel_id);
+  channel.send(message);  
 }
